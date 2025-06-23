8fcdaefb0c760fff5be50fe1ca9feecdfcff6bab
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-06-23 09:44:37 +0800

    improved documentation

--- R/create_table_3.R
+++ R/create_table_3.R
@@ -3,16 +3,19 @@
 #' @param project_path The path to the directory of the project to generate the
 #'  tables for, defaults to the current working directory and recurses into
 #'  subfolders.
-#' @param file_path The path to the directory where the table will be saved,
-#'  defaults to the project's "1 Documentation" directory.
-#' @param quarto Logical, whether to cite the \acronym{AAGI} Quarto template.
+#' @param file_path Character string that provides the file path to the
+#'  directory where the table will be saved. Defaults to the current project's
+#'  \dQuote{1 Documentation} directory.
+#' @param quarto Logical, whether to site the \acronym{AAGI} Quarto template.
 #' Defaults to `FALSE`.
-#'
+#' @param quarto Boolean cite the \acronym{AAGI} Quarto template? Defaults to
+#'  `FALSE`.
+#' @param digger Boolean cite the \pkg{DiGGer} package? Defaults to `FALSE`.
 #' @examples
 #' create_table_3()
 #'
-#' @return nothing, called for its side effects of writing an Excel file to disk
-#' for inclusion in the IPPO document spreadsheet.
+#' @return An invisible `NULL, called for its side effects of writing an Excel
+#'  workbook file to disk for inclusion in the IPPO document workbook.
 #'
 #' @author Zhanglong Cao, \email{zhanglong.cao@@curtin.edu.au} and Adam H.
 #' Sparks, \email{adam.sparks@@curtin.edu.au}
@@ -29,15 +32,15 @@ create_table_3 <- function(
   }
 
   # Set the working directory to the specified path
-  withr::withdir(project_path)
+  withr::with_dir(project_path)
 
-  pkgs <- funspottr::spot_packages(
+  pkgs <- funspotr::spot_pkgs_files(
     path = project_path,
-    recursive = TRUE
+    .progress = TRUE
   )
 
   if (digger) {
-    digger <- data.frame(
+    digger <- tibble::tibble(
       Package = "DiGGer",
       Author = "Neil Coombes [aut, ctb]",
       Description = "Searches for efficient experimental designs under specified blocking and correlation.",

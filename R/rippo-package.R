a7e1dc247d87f8cc5de8445dc03c8ddf5c0e151d
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-06-23 14:50:50 +0800

    remove data.table from package

--- R/rippo-package.R
+++ R/rippo-package.R
@@ -1,16 +1,4 @@
 #' @keywords internal
 "_PACKAGE"
 
-## usethis namespace: start
-#' @importFrom data.table :=
-#' @importFrom data.table .BY
-#' @importFrom data.table .EACHI
-#' @importFrom data.table .GRP
-#' @importFrom data.table .I
-#' @importFrom data.table .N
-#' @importFrom data.table .NGRP
-#' @importFrom data.table .SD
-#' @importFrom data.table data.table
-#' @importFrom data.table %notin%
-## usethis namespace: end
 NULL

b03f07f71306e1e86925f88b46cd0feec1833050
Braden-Thorne <braden.thorne@research.uwa.edu.au>
2025-06-30 12:29:01 +0800

    Replaced stop() with cli::cli_abort()

--- R/run_IPPO_app.R
+++ R/run_IPPO_app.R
@@ -15,11 +15,11 @@ run_IPPO_app <- function() {
   installed_pkgs <- rownames(installed.packages())
   not_found <- setdiff(c("shiny", "shinyWidgets", "bslib", "dplyr", "openxlsx"), grep("(^shiny$)|(^shinyWidgets$)|(^bslib$)|(^dplyr$)|(^openxlsx$)", installed_pkgs, value = TRUE))
   if (length(not_found) > 0) {
-    stop("Package ", not_found[1], " is needed for this function to work. Please install it.", call. = FALSE)
+    cli::cli_abort("Package ", not_found[1], " is needed for this function to work. Please install it.", call. = FALSE)
   }
   
   if (appDir == "") {
-    stop("Could not find IPPO_app. Try re-installing `rippo`.", call. = FALSE)
+    cli::cli_abort("Could not find IPPO_app. Try re-installing `rippo`.", call. = FALSE)
   }
   else {
     ### Can add a resource path here if required using shiny::addResourcePath('path', system.file('path', package = 'rippo'))

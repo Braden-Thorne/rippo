5a7ca7509ce49c6a752d63e058b692ce68c29bfd
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-07-29 10:57:07 +0800

    remove redundant code

--- inst/update_register.R
+++ inst/update_register.R
@@ -7,8 +7,6 @@ library(fs)
 sp <- "CU" # strategic partner for report
 R_drive <- "/Volumes/dmp/A-J/AAGI_CCDM_CBADA-GIBBEM-SE21982/"
 
-ippo_doc <- "~/Library/CloudStorage/Box-Box/AAGI-GRDC/AAGI IPPO Register/AAGI-CU-IPPO Register.docx"
-
 tl <- list_ippo_tables(
     dir_path_in = path(R_drive, "Projects"),
     sp = sp
